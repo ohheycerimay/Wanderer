3d9414deeb460452a9848dd05e50fa1c44288860
Ceri Dorsey-Tyler <cerimay@gmail.com>
2022-12-22 13:42:11 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Destroying database"
+
+User.destroy_all
+Experience.destroy_all
+Booking.destroy_all
+
+puts "Seeding users...👥"
+
+User.create(username:"ohheycerimay", password_digest:"1234", image_url:"https://www.kateblackburn.us/wp-content/uploads/2017/09/jp_12182015_nyt_crossword.jpg")
+User.create(username:"luimatthew", password_digest:"5678", image_url:"https://media.istockphoto.com/id/1309040743/photo/travel-planning-background.jpg?b=1&s=170667a&w=0&k=20&c=oYsf4vJ1lCmKJAwq1DAIte1KH56qCmiyNDvoysDHUhQ=")
+User.create(username:"paigetetz", password_digest:"9876", image_url:"http://cdn.akc.org/content/article-body-image/siberian_husky_cute_puppies.jpg")
+
+puts "Users seeded!"
+
+puts "Seeding experiences..."
+
+Experience.create(location:"", description:"", price:, image_url:"", avaliability:, includes:"")
+Experience.create(location:"", description:"", price:, image_url:"", avaliability:, includes:"")
+Experience.create(location:"", description:"", price:, image_url:"", avaliability:, includes:"")
+Experience.create(location:"", description:"", price:, image_url:"", avaliability:, includes:"")
+Experience.create(location:"", description:"", price:, image_url:"", avaliability:, includes:"")
+Experience.create(location:"", description:"", price:, image_url:"", avaliability:, includes:"")
+Experience.create(location:"", description:"", price:, image_url:"", avaliability:, includes:"")
+Experience.create(location:"", description:"", price:, image_url:"", avaliability:, includes:"")
+Experience.create(location:"", description:"", price:, image_url:"", avaliability:, includes:"")
+Experience.create(location:"", description:"", price:, image_url:"", avaliability:, includes:"")
+Experience.create(location:"", description:"", price:, image_url:"", avaliability:, includes:"")
+Experience.create(location:"", description:"", price:, image_url:"", avaliability:, includes:"")
+Experience.create(location:"", description:"", price:, image_url:"", avaliability:, includes:"")
+Experience.create(location:"", description:"", price:, image_url:"", avaliability:, includes:"")
+Experience.create(location:"", description:"", price:, image_url:"", avaliability:, includes:"")
+
+puts "Experiences seeded!"
+
+puts "Seeding bookings..."
+
+Booking.create(user_id:, experience_id:, start_date:, end_date:, total_price:)
+Booking.create(user_id:, experience_id:, start_date:, end_date:, total_price:)
+Booking.create(user_id:, experience_id:, start_date:, end_date:, total_price:)
+Booking.create(user_id:, experience_id:, start_date:, end_date:, total_price:)
+Booking.create(user_id:, experience_id:, start_date:, end_date:, total_price:)
+Booking.create(user_id:, experience_id:, start_date:, end_date:, total_price:)
+
+puts "Bookings seeded!"
