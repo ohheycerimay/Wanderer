73d1e61e80fcdcdff33e806dcaeecc70a27806ef
paige <paigetetz@gmail.com>
2023-01-03 21:49:42 -0500

    Logo added

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Search from './Search';
-
+import Wander from './Wander.png';
 
 function Home({search, setSearch}) {
     
     return (
+        <div>
+            <img src={Wander} alt="Wander" />
         <div className='search_div' div>
             <h2>Start your journey!</h2>
             <Search search={search} setSearch={setSearch}  />
         
-            
+        </div>   
         </div>
     );
 }
